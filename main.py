dbe79979f9b59ea81c2614976b2650b191d0beb9
vcokltfre <vcokltfre@gmail.com>
2021-03-13 04:01:46 +0000

    chore: formatting fixes

--- main.py
+++ main.py
@@ -34,13 +34,14 @@ async def get_sf(snowflake: int):
 
     rb = ResponseBuilder()
     rb.addtag("title", f"Snowflake: {snowflake}")
-    desc = "**__Raw Data:__**\n"
+    desc = "Raw Data:\n"
     desc += f"{getsfdata(sf)}\n\n"
-    desc += f"**__ISO 8601 Format:__**\n"
+    desc += f"ISO 8601 Format:\n"
     desc += f"{date.isoformat()}\n\n"
-    desc += f"**__Human Time:__**\n"
+    desc += f"Human Time:\n"
     desc += f"{humanize.naturaldate(date.date())}"
     rb.addtag("description", desc)
+    rb.addtag("url", "https://vcokltf.re")
 
     return rb.build()
 
@@ -51,12 +52,13 @@ async def get_sf(snowflake_1: int, snowflake_2: int):
 
     rb = ResponseBuilder()
     rb.addtag("title", f"Compare: {snowflake_1} | {snowflake_2}")
-    desc = f"**__Data for {snowflake_1}:__**\n"
+    desc = f"Data for {snowflake_1}:\n"
     desc += f"{getsfdata(sf1)}\n\n"
-    desc += f"**__Data for {snowflake_2}:__**\n"
+    desc += f"Data for {snowflake_2}:\n"
     desc += f"{getsfdata(sf2)}\n\n"
-    desc += f"**__Difference:__**\n"
+    desc += f"Difference:\n"
     desc += f"{humanize.naturaldelta((max(sf1.timestamp, sf2.timestamp) // 1000) - (min(sf1.timestamp, sf2.timestamp) // 1000))}"
     rb.addtag("description", desc)
+    rb.addtag("url", "https://vcokltf.re")
 
     return rb.build()
